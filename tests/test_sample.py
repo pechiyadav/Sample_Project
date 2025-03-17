97333324b764493319e67a1ef69fb10ee2bdd286
pechiyadav <77777433+pechiyadav@users.noreply.github.com>
2025-03-17 11:25:24 +0530

    Created test file named test_sample.py

---
+++ tests/test_sample.py
@@ -0,0 +1,11 @@
+import pytest
+from main import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_failure_example():
+    assert subtract(5, 2) == 1  # Intentional failure for demonstration
