ba5a0b6e52aa23b60f087c202f946b3b408bdb1b
pechiyadav <77777433+pechiyadav@users.noreply.github.com>
2025-03-17 11:58:11 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -7,5 +7,5 @@ def test_add():
 def test_subtract():
     assert subtract(5, 3) == 2
 
-#def test_failure_example():
-#    assert subtract(5, 2) == 1  # Intentional failure for demonstration
+def test_failure_example():
+    assert subtract(5, 2) == 1  # Intentional failure for demonstration
